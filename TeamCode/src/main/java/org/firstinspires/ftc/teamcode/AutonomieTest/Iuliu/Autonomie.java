14b1889e15e80b2749ec3b510ce3bdff34d5ac7b
Iuliu27 <133576069+Iuliu27@users.noreply.github.com>
2023-08-30 20:53:42 +0300

    Am adaugat MeepMeepTesting.java am facut foldere pentru fiecare dintre noi. si atat

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomieTest/Iuliu/Autonomie.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomieTest/Iuliu/Autonomie.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.AutonomieTest;
+package org.firstinspires.ftc.teamcode.AutonomieTest.Iuliu;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
