202146369f0cc9b8e653643c07a9389cd7f0bcf5
Iuliu27 <133576069+Iuliu27@users.noreply.github.com>
2023-08-30 19:39:02 +0300

    Autonomie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomieTest/Autonomie.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomieTest/Autonomie.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.AutonomieTest;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Autonomie extends LinearOpMode {
 
-
+    DcMotor motor;
     @Override
     public void runOpMode() throws InterruptedException {
 
