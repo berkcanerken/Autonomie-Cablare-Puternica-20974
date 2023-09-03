45d41b2549b1cd00ebfa0abd1682b0cd1c8c74ae
Iuliu27 <133576069+Iuliu27@users.noreply.github.com>
2023-09-03 22:17:30 +0300

    tot asta

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomieTest/Iuliu/Opmodes/BlueConeDetectorClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomieTest/Iuliu/Opmodes/BlueConeDetectorClass.java
@@ -1,82 +1,56 @@
 package org.firstinspires.ftc.teamcode.AutonomieTest.Iuliu.Opmodes;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.teamcode.TuningAndTrajectories.TuningConstants.SampleMecanumDrive.*;
+import static org.firstinspires.ftc.teamcode.TuningAndTrajectories.TuningConstants.SampleMecanumDrive.servo;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.AutonomieTest.Iuliu.BlueConeDetector.Classes.BlueConeDetector;
-
+import org.firstinspires.ftc.teamcode.software.OpenCV.initCamera;
 import org.opencv.core.Point;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
-
 @Autonomous(name = "ColorDetectionAutonomous", group = "Iuliu")
 public class BlueConeDetectorClass extends LinearOpMode {
+
+    ElapsedTime timer;
+
     OpenCvCamera externalCamera;
-    public static BlueConeDetector pipeline;
+     public static BlueConeDetector pipeline;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        initExternalCamera();
-        pipeline = new BlueConeDetector();
+        // Initialize the hardwareMap
 
+
+        // Create an instance of initCamera
+        initCamera camera = new initCamera();
+
+        // Call the initExternalCamera method on the camera instance
+        camera.initExternalCamera();
+
+        // Initialize the camera and pipeline
+        externalCamera = camera.externalCamera;
+        pipeline = new BlueConeDetector();
 
         waitForStart();
 
         while (opModeIsActive()) {
-            if (pipeline.isBlueDetected())
-            {
+            if (pipeline.isBlueDetected()) {
+                // Assuming you have a servo object defined elsewhere in your code
                 servo.setPosition(1);
-            } else
-            {
+            } else {
                 servo.setPosition(0);
             }
+            Point squareCenter = pipeline.getSquareCenter(); // Get the center coordinates
 
-
-            ComposeTelementry();
+            telemetry.addData("Square Center X", squareCenter.x); // Display X coordinate
+            telemetry.addData("Square Center Y", squareCenter.y); // Display Y coordinate
+            telemetry.addData("Servo Position", servo.getPosition());
             telemetry.update();
         }
-
-        if(isStopRequested())return;
     }
-    private void ComposeTelementry()
-    {
-        Point squareCenter = pipeline.getSquareCenter();
-        telemetry.addData("Square Center X", squareCenter.x); // Display X coordinate
-        telemetry.addData("Square Center Y", squareCenter.y); // Display Y coordinate
-        telemetry.addData("Servo Position", servo.getPosition());
-    }
-    public void initExternalCamera() {
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName()
-        );
-
-        externalCamera = OpenCvCameraFactory.getInstance().createWebcam(
-                hardwareMap.get(WebcamName.class, "WebcamG"), cameraMonitorViewId
-        );
-        /*
-        TODO when you will use this in a rel progect implement here all piplines
-
-        */
-
-        pipeline = new BlueConeDetector();
-        externalCamera.setPipeline(pipeline);
-
-        externalCamera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                externalCamera.startStreaming(320, 240, OpenCvCameraRotation.UPSIDE_DOWN);
-            }
-
-            @Override
-            public void onError(int errorCode) {
-
-            }
-        });
-    }
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/software/OpenCV/initCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/software/OpenCV/initCamera.java
@@ -2,18 +2,18 @@ package org.firstinspires.ftc.teamcode.software.OpenCV;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import static org.firstinspires.ftc.teamcode.AutonomieTest.Iuliu.Opmodes.BlueConeDetectorClass.pipeline;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.AutonomieTest.Iuliu.BlueConeDetector.Classes.BlueConeDetector;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
-
- class initCamera {
+ public class initCamera {
      public OpenCvCamera externalCamera;
 
 
-    private void initExternalCamera() {
+    public void initExternalCamera() {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
                 "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName()
         );
