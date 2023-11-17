1e08dd3d8d8904ab6f4225f5f02a034f052da264
Mara2oo7 <142086211+Mara2oo7@users.noreply.github.com>
2023-11-17 22:34:40 +0200

    Am facut teleopul din nou ca la sters iulio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomieTest/Mara/TeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomieTest/Mara/TeleOp1.java
@@ -61,7 +61,7 @@ public class TeleOp1 extends LinearOpMode {
                 telemetry.addData("Razvi ","Esti un figurant");
                 telemetry.update();
             }
-            //giumbusluc
+            //giumbusluc23
 
             if (gamepad2.dpad_up) y = 0.5;
             if (gamepad2.dpad_down) y = -0.5;
