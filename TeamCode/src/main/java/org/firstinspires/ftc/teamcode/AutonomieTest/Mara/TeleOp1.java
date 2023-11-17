f68dca7af18bd8e14798f9a602c9facf1db8f660
Mara2oo7 <142086211+Mara2oo7@users.noreply.github.com>
2023-11-17 22:24:17 +0200

    Am facut teleopul din nou ca la sters iulio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomieTest/Mara/TeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomieTest/Mara/TeleOp1.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode.AutonomieTest.Mara;
 
 import static org.firstinspires.ftc.teamcode.AutonomieTest.Mara.constants.*;
+import static org.firstinspires.ftc.teamcode.AutonomieTest.Mara.constants.backLeftPower;
+import static org.firstinspires.ftc.teamcode.AutonomieTest.Mara.constants.backRightPower;
+import static org.firstinspires.ftc.teamcode.AutonomieTest.Mara.constants.frontLeftPower;
+import static org.firstinspires.ftc.teamcode.AutonomieTest.Mara.constants.frontRightPower;
 import static org.firstinspires.ftc.teamcode.TuningAndTrajectories.TuningConstants.SampleMecanumDrive.servol;
 import static org.firstinspires.ftc.teamcode.TuningAndTrajectories.TuningConstants.SampleMecanumDrive.servor;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.TuningAndTrajectories.TuningConstants.SampleMecanumDrive;
@@ -20,13 +25,20 @@ public class TeleOp1 extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         drive = new SampleMecanumDrive(hardwareMap);
-        backLeftMotor = (DcMotor) hardwareMap.get(constants.class, "motorLB");
-        frontLeftMotor = (DcMotor) hardwareMap.get(constants.class, "motorLF");
-        backRightMotor = (DcMotor) hardwareMap.get(constants.class, "motorRB");
-        frontLeftMotor = (DcMotor) hardwareMap.get(constants.class, "motorRF");
+        DcMotorEx backLeftMotor = hardwareMap.get(DcMotorEx.class, "motorLB");
+        DcMotorEx backRightMotor = hardwareMap.get(DcMotorEx.class, "motorRB");
+        DcMotorEx frontLeftMotor = hardwareMap.get(DcMotorEx.class, "motorLF");
+        DcMotorEx frontRightMotor = hardwareMap.get(DcMotorEx.class, "motorRF");
+        DcMotorEx mbrat = hardwareMap.get(DcMotorEx.class,"mbrat");
 
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mbrat.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         double y = -gamepad1.left_stick_y / 5;
         double x = -gamepad1.left_stick_x / 5;
@@ -35,43 +47,46 @@ public class TeleOp1 extends LinearOpMode {
 
         //ServoDS, ServoDD, ServoIS, ServoID, PowerX, PowerY
         while (opModeIsActive()) {
-            //gamepad1 sa foloseasca miscarea bratului si a servourilor
-            //gamepad2 sa foloseasca miscarea robotului
-            if (gamepad1.a) {
+            if (gamepad2.a) {
                 servol.setPosition(ServoDS);
                 servor.setPosition(ServoDD);
             }
-            if (gamepad1.b) {
+            if (gamepad2.b) {
                 servol.setPosition(ServoIS);
                 servor.setPosition(ServoID);
             }
 
-            if (gamepad2.dpad_up) {
-                y = 0.5;
+            if(gamepad2.x && gamepad2.y)
+            {
+                telemetry.addData("Razvi ","Esti un figurant");
+                telemetry.update();
             }
-            if (gamepad2.dpad_down) {
-                y = -0.5;
-            }
-            if (gamepad2.dpad_right) {
-                x = -0.5;
-            }
-            if (gamepad2.dpad_left) {
-                x = 0.5;
-            }
+
+            if (gamepad2.dpad_up) y = 0.5;
+            if (gamepad2.dpad_down) y = -0.5;
+            if (gamepad2.dpad_right) x = -0.5;
+            if (gamepad2.dpad_left) x = 0.5;
+
+            if(gamepad2.right_trigger>0) mbrat.setPower(-gamepad2.right_trigger);
+            if(gamepad2.left_trigger>0) mbrat.setPower(gamepad2.left_trigger);
+
+            double rx = gamepad1.right_stick_x * 0.5;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y - x + rx) / denominator;
+            double backLeftPower = (y + x + rx) / denominator;
+            double frontRightPower = (y + x - rx) / denominator;
+            double backRightPower = (y - x - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+
+            telemetry.clearAll();
+            telemetry.update();
+
         }
-
-        double rx = gamepad1.right_stick_x * 0.5;
-
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double frontLeftPower = (y - x + rx) / denominator;
-        double backLeftPower = (y + x + rx) / denominator;
-        double frontRightPower = (y + x - rx) / denominator;
-        double backRightPower = (y - x - rx) / denominator;
-
-        frontLeftMotor.setPower(frontLeftPower);
-        backLeftMotor.setPower(backLeftPower);
-        frontRightMotor.setPower(frontRightPower);
-        backRightMotor.setPower(backRightPower);
 
         if (isStopRequested()) {
             return;
